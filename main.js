c2b85f761757a81dc1c642bd98b6da3458161ea9
Eddy Ruiz <98863028+eddyrdev@users.noreply.github.com>
2023-03-28 08:22:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,17 +4,15 @@ const imgCat = document.querySelector('#img-cat');
 btnLoad.addEventListener('click', loadImg);
 async function loadImg() {
   try {
-    const response = await fetch('https://cataas.com/cat');
-
-    if (!response.ok) {
+    const res = await fetch('https://cataas.com/cat');
+    if (!res.ok) {
       throw new Error('Could not load image');
     } else {
-      const binary = await response.blob();
+      const binary = await res.blob();
       imgCat.src = URL.createObjectURL(binary);
       console.log('Loaded image');
     }
   } catch (error) {
     console.error(error);
-    alert('An error occurred while loading the image');
   }
 }
