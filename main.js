ea88c5130023cb1c08283bdc31e6dfc1cc813dee
Eddy Ruiz <98863028+eddyrdev@users.noreply.github.com>
2023-03-28 07:54:26 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-const btnLoad = document.querySelector('#btn-load');
-const imgCat = document.querySelector('#img-cat');
-btnLoad.addEventListener('click', loadImg);
+<!DOCTYPE html>
+<html lang="en">
 
-async function loadImg() {
-  try {
-    const response = await fetch('https://cataas.com/cat');
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Asincronia en JS</title>
+</head>
 
-    if (!response.ok) {
-      throw new Error('No se pudo cargar la imagen');
-    }
+<body>
+  <div id="titulo">
+    <img src="cat.png" alt="Descripción de la imagen" style="margin-right: 20px;">
+    <h1>CATAAS</h1>
+    <img src="cat.png" alt="Descripción de la imagen" style="margin-left: 20px;">
+  </div>
+  <div>
+    <center>
+      <img src="noimage.png"  id="img-cat" alt="Imagen de gato al azar">
+    </center>
+  </div>
+  <div>
+    <p>Presiona el siguiente boton para cargar una imagen aleatoria</p>
+    <center><button id="btn-load" class="button primary color2">CARGA IMAGEN</button></center>
+  </div>
+  <footer>&copy; Eddy Ruiz y Abigail Obrajero 2023</footer>
+  <script src="./main.js"></script>
+</body>
 
-    const binary = await response.blob();
-
-    imgCat.src = URL.createObjectURL(binary);
-
-  } catch (error) {
-    console.error(error);
-    alert('Ocurrió un error al cargar la imagen');
-  }
-}
+</html>
